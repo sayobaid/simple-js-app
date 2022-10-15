b6670a7b01560b719a9cc248eba06351cfa635f2
Obaid Alam <sayobaid@gmail.com>
2022-10-15 16:26:47 +0530

    Add files via upload
    PHP Changes

---
+++ index.php
@@ -0,0 +1,34 @@
+<html>
+    <head>
+        <title>Todo List App</title>
+        <link href="style.css" rel="stylesheet">
+    </head>
+    <body>
+        <center>
+            <div id="heading">
+                Todo List
+          </div>
+        </center>
+        <hr>
+        <div id="alltodo">
+
+
+        </div>
+       
+
+<br>
+
+<br>
+<br>
+<br>
+<br>
+<br>
+        <div id="todoform">
+            Add a new Todo
+            <input type="text" id="newtodotitle" placeholder="Todo Title">
+            <input type="text" id="newtodocontent" placeholder="Todo Content">
+            <button onclick="addNewTodo()">Add Todo</button>
+        </div>
+    </body>
+</html>
+<script src="todo.js"></script>
